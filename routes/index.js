5445c5a34901f00a6425f5934224300f2b7ba809
Oriol Testart <orioltestart@gmail.com>
2017-01-16 16:10:41 +0100

    ProcessaURL

--- routes/index.js
+++ routes/index.js
@@ -23,9 +23,8 @@ function check (file, req, res) {
     res.redirect('/no-git')
     return
   }
-  if (!file) {
+  if (!fs.existsSync(file)) {
     res.redirect('/404')
-    return
   }
 }
 
@@ -33,60 +32,32 @@ router.get('/404', function (req, res) {
   res.render('404')
 })
 
-/*router.param('path', function (req, res, next, path) {
+function processaURL (req, res, next) {
   // Controlem que tingui el programari correctament instalat
-  if (path.match('favicon')) {
-    return
-  }
-  check(path, req, res)
+  check(req.url, req, res)
 
-  let full_path = GLOBAL_PATH + path
+  let full_path = req.url
   console.log('### fullpath param1: ' + full_path)
 
   if (fs.lstatSync(full_path).isDirectory()) {
     let command = shell.exec('ls ' + full_path).stdout.split('\n').slice(0, -1)
     console.log('### command: ' + command)
     res.locals.fitxers = command
-    res.locals.path = path
     res.locals.isDirectory = true
-		// res.render('vista_web',{
-			// fitxers: command,
-			// pare: path
-		// })
   } else {
     let command = shell.exec('git status')
     let path2 = shell.exec('git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --follow ' + path)
 
     res.locals.pwd = path2.split('\n').map(dividirCommits)
-    res.locals.path = path
+    res.locals.path = req.url
     res.locals.historial_fitxer = command
     res.locals.isDirectory = false
-
-		// res.render('vista_fitxer',{
-			// fitxers: command,
-			// title: 'GIT!',
-			// path: res.locals.pwd,
-			// file: res.locals.path
-		// })
   }
-
-	// let command = "find . \\( -path './node_modules' -o -path './bower_components' -o -path './data' \\) -prune -o -iname '"+ file +"' -print"
-	// let aux = shell.exec(command).stdout
-	// console.log(aux)
-	// if (!aux) {
-		// res.redirect('404')
-		// return
-	// }
-
-	// let path = shell.exec('git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --follow ' + aux)
-	// res.locals.pwd = path.split('\n').map(dividirCommits)
-	// res.locals.file = file
   next()
-})*/
+}
 
 //  ROUTES
-router.get('/webs/:path*?', function (req, res) {
-	// if(fs.lstatSync(full_path).isDirectory()){
+router.get('/webs/:path*?', processaURL, function (req, res) {
   if (res.locals.isDirectory) {
     console.log('### mostrar fitxers web: ' + res.locals.path)
     res.render('vista_web', {
