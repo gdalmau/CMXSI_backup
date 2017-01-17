356048abf64801da3ce38aca47083abafa5720c7
root <root@orioltestart.com>
2017-01-17 10:41:23 +0000

    Updates

--- public/js/main.js
+++ public/js/main.js
@@ -23,89 +23,134 @@ jQuery(document).ready(function ($) {
       ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden')) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in')
     })
   }
-	
-  $('.restore').on('click', function (e) {
-    e.preventDefault()
-    let commit = $(e.currentTarget).attr('tag')
-    let path = $('#path').text()
-    $.post({
-      url: '/restore',
-      data: {
-        commit_id: commit,
-        path: path
-      }
-    }).done(function(data){
-		console.log(data)
-		toastr.info(data)
-		setTimeout(location.reload(),5000)
-	})
-  })
-  
-  $('.diff').on('click', function (e) {
-    e.preventDefault()
-    let commit = $(e.currentTarget).attr('tag')
-    let path = $('#path').text()
-    $.post({
-      url: '/show_diff',
-      data: {
-        commit_id: commit,
-        path: path
-      }
-    }).done(function(data){
-		console.log(data.missatge)
-		console.log(data.result)
-		$(e.currentTarget).append(data.result)
-		toastr.info(data.missatge)
-		// setTimeout(location.reload(),5000)
-	})
-  })
-  
-  $('.diff_tree').on('click', function (e) {
-    e.preventDefault()
-    let commit = $(e.currentTarget).attr('tag')
-    let path = $('#path').text()
-    $.post({
-      url: '/show_diff_tree',
-      data: {
-        commit_id: commit,
-        path: path
-      }
-    }).done(function(data){
-		console.log(data.missatge)
-		console.log(data.result)
-		$(e.currentTarget).append(data.result)
-		toastr.info(data.missatge)
-		// setTimeout(location.reload(),5000)
-	})
-  })
 })
+
+function restore_web(e) {
+    e.preventDefault()
+    let commit = $(e.currentTarget).attr('tag')
+    let path = $('#path').text()
+    $.post({
+      url: '/restore_web',
+      data: {
+        commit_id: commit,
+        path: path
+      }
+    }).done(function (data) {
+      console.log(data)
+      toastr.info(data)
+      window.setTimeout(function () {
+        window.location.reload()
+      }, 5000)
+	})
+}
+
+function diff_web (e) {
+    e.preventDefault()
+    swal({
+      title: "N'estas segur?",
+      text: 'You will not be able to recover this imaginary file!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#DD6B55',
+      confirmButtonText: 'Si, recupera\'l',
+      cancelButtonText: 'Cancela',
+      closeOnConfirm: true
+    }, function () {
+        let commit = $(e.currentTarget).attr('tag')
+        let path = $('#path').text()
+        $.post({
+          url: '/show_diff_web',
+          data: {
+            commit_id: commit,
+            path: path
+        }
+        }).done(function (data) {
+          console.log(data.missatge)
+          console.log(data.result)
+          $(e.currentTarget).append(data.result)
+          toastr.info(data.missatge)
+          window.setTimeout(function () {
+            window.location.reload()
+          }, 5000)
+        })
+      swal('Hem recuperat el fitxer al seu estat anterior', 'success')
+    })
+}
+
+function diff_tree_web(e) {
+    e.preventDefault()
+    let commit = $(e.currentTarget).attr('tag')
+    let path = $('#path').text()
+    $.post({
+      url: '/show_diff_tree_web',
+      data: {
+        commit_id: commit,
+        path: path
+      }
+    }).done(function(data){
+		console.log(data.missatge)
+		console.log(data.result)
+		$(e.currentTarget).append(data.result)
+		toastr.info(data.missatge)
+      window.setTimeout(function () {
+        window.location.reload()
+      }, 5000)
+	})
+}
 
 function backup_web(e){
 	e.preventDefault()
     let path = $('#path').text()
     $.post({
-      url: '/backup',
+      url: '/backup_web',
       data: {
         path: path
       }
     }).done(function(data){
 		console.log(data)
 		toastr.info(data)
-		setTimeout(location.reload(),5000)
+      window.setTimeout(function () {
+        window.location.reload()
+      }, 5000)
 	})
 }
 
-function undo_restore(e){
+function undo_restore_web(e) {
 	e.preventDefault()
     let path = $('#path').text()
     $.post({
-      url: '/undo_restore',
+      url: '/undo_restore_web',
       data: {
         path: path
       }
     }).done(function(data){
 		console.log(data)
 		toastr.info(data)
-		setTimeout(location.reload(),5000)
+      window.setTimeout(function () {
+        window.location.reload()
+      }, 5000)
+	})
+}
+
+function restore_file(e) {
+    e.preventDefault()
+    let commit = $(e.currentTarget).attr('tag')
+	let full_path = $('#path').text()
+	let full_path_splitted = full_path.split('/')
+    let nom_web = full_path_splitted[2]
+	let file = full_path_splitted[full_path_splitted.length-1]
+    $.post({
+      url: '/restore_file',
+      data: {
+        commit_id: commit,
+        nom_web: nom_web,
+		file_name: file
+      }
+    }).done(function(data){
+		console.log(data)
+		toastr.info(data)
+      window.setTimeout(function () {
+        window.location.reload()
+      }, 5000)
 	})
 }
--- routes/index.js
+++ routes/index.js
@@ -23,8 +23,8 @@ function check (file, req, res) {
     res.redirect('/no-git')
     return
   }
-  let check = file.replace('/webs',GLOBAL_PATH)
-  console.log('checking: '+check)
+  let check = file.replace('/webs', GLOBAL_PATH)
+  console.log('checking: ' + check)
   if (!fs.existsSync(check)) {
     res.redirect('/404')
   }
@@ -37,44 +37,23 @@ router.get('/404', function (req, res) {
 
 function processaURL (req, res, next) {
   // Controlem que tingui el programari correctament instalat
-  
-  let full_path = check(req.url, req, res)
-  console.log('### fullpath param1: ' + full_path)
-  console.log('req.url route: '+req.url)
-  
-  let nom_web = GLOBAL_PATH+'/'+req.url.split('/')[2]
-  
-  
-  if (fs.lstatSync(full_path).isDirectory()) {
-    let command_ls = shell.exec('ls ' + full_path).stdout.split('\n').slice(0, -1)
-    console.log('### command: ' + command_ls)
-    res.locals.fitxers = command_ls
+
+  let fullPath = check(req.url, req, res)
+  let nomWeb = GLOBAL_PATH + '/' + req.url.split('/')[2]
+
+  shell.cd(nomWeb)
+  if (fs.lstatSync(fullPath).isDirectory()) {
+    res.locals.fitxers = shell.exec('ls ' + fullPath).stdout.split('\n').slice(0, -1)
     res.locals.isDirectory = true
-	res.locals.route = req.url
-	
-	let command_cd = shell.cd(nom_web)
-	let command_log = 'git log --pretty=format:"%h%x09%an%x09%ad%x09%s"'
-	let historial = shell.exec(command_log)
-	let commits = historial.split('\n').map(dividirCommits)
-	res.locals.commits = commits
+    res.locals.route = req.url
+
+    let historial = shell.exec('git log --pretty=format:"%h%x09%an%x09%ad%x09%s"')
+    let commits = historial.split('\n').map(dividirCommits)
+    res.locals.commits = commits
   } else {
-    // let command = shell.exec('git status')
-    // let command = shell.exec('git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --follow ' + full_path)
-	// let command = shell.exec('git log' + full_path)
-	
-	let command_cd = shell.cd(nom_web)
-	let command_log = 'git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --follow ' + full_path
-	let historial = shell.exec(command_log)
-	let commits = historial.split('\n').map(dividirCommits)
-	//command = 'git diff --no-commit-id -r '+ commits[2]['commit']
-	//console.log("\n\n"+command)
-	//let command_diff = shell.exec(command)
-	//console.log("\n\nCommand DIFF: " + command_diff)
-	
-	
-	
-	//console.log("Command historial: "+command_cd + '\n'+command)
-	//console.log("Command historial total: "+historial)
+    let historial = shell.exec('git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --follow ' + fullPath)
+    let commits = historial.split('\n').map(dividirCommits)
+
     res.locals.commits = commits
     res.locals.route = req.url
     res.locals.isDirectory = false
@@ -82,84 +61,119 @@ function processaURL (req, res, next) {
   next()
 }
 
-//  ROUTES
+// WEB ROUTES
 router.get('/webs/:path*?', processaURL, function (req, res) {
   if (res.locals.isDirectory) {
-    console.log('### mostrar fitxers web: ' + res.locals.route)
-	
     res.render('vista_web', {
       fitxers: res.locals.fitxers,
       pare: res.locals.route,
-	  commits: res.locals.commits
+      commits: res.locals.commits,
+      breadcrums: res.locals.route.split('/')
     })
   } else {
     console.log('### mostrar fitxer: ' + res.locals.path)
     res.render('vista_fitxer', {
       title: 'GIT!',
       file: res.locals.route,
-	  commits: res.locals.commits
+      commits: res.locals.commits
     })
   }
 })
 
-router.post('/show_diff', function(req, res) {
-	let full_path = req.body.path
-	let commit_id = req.body.commit_id
-	let nom_web = check(full_path, req, res)
-	let command_cd = shell.cd(nom_web)
-	let command_git_diff = shell.exec('git diff --no-commit-id '+commit_id)
-	res.send({
-		result: command_git_diff,
-		missatge: 'Mostrats canvis del commit '+commit_id+' de la web '+nom_web+' fet!'
-	})
+router.post('/show_diff_web', function(req, res) {
+  console.log('\n==========================\n')
+  console.log('## REALITZANT DIFF      ##\n')
+  console.log('==========================\n')
+  let fullPath = req.body.path
+  let commitId = req.body.commit_id
+  let nomWeb = check(fullPath, req, res)
+  shell.cd(nomWeb)
+  let gitDiff = shell.exec('git diff --no-commit-id ' + commitId)
+  res.send({
+    result: gitDiff,
+    missatge: 'Mostrats canvis del commit ' + commitId + ' de la web ' + nomWeb + ' fet!'
+  })
 }) 
 
-router.post('/show_diff_tree', function(req, res) {
-	let full_path = req.body.path
-	let commit_id = req.body.commit_id
-	let nom_web = check(full_path, req, res)
-	let command_cd = shell.cd(nom_web)
-	let command_git_diff_tree = shell.exec('git diff-tree --no-commit-id --name-only -r '+commit_id)
-	res.send({
-		result: command_git_diff_tree,
-		missatge: 'Mostrats fitxers canviats del commit '+commit_id+' de la web '+nom_web+' fet!'
-	})
+router.post('/show_diff_tree_web', function(req, res) {
+  console.log('\n==========================\n')
+  console.log('## REALITZANT DIFF TREE ##\n')
+  console.log('==========================\n')
+  let fullPath = req.body.path
+  let commitId = req.body.commit_id
+  let nomWeb = check(fullPath, req, res)
+  shell.cd(nomWeb)
+  let diffTree = shell.exec('git diff-tree --no-commit-id --name-only -r ' + commitId)
+  res.send({
+    result: diffTree,
+    missatge: 'Mostrats fitxers canviats del commit ' + commitId + ' de la web ' + nomWeb + ' fet!'
+  })
+  console.log('==========================\n')
 }) 
 
-router.post('/restore', function(req, res) {
-	let full_path = req.body.path
-	let commit_id = req.body.commit_id
-	let nom_web = check(full_path, req, res)
-	let command_cd = shell.cd(nom_web)
-	let command_git_checkout = shell.exec('git checkout '+commit_id)
-	res.send('Restaurat commit '+commit_id+' de la web '+nom_web+' fet!')
+router.post('/restore_web', function (req, res) {
+  console.log('\n==========================\n')
+  console.log('## REALITZANT RESTORE   ##\n')
+  console.log('==========================\n')
+  let fullPath = req.body.path
+  let commitId = req.body.commit_id
+  let nomWeb = check(fullPath, req, res)
+  shell.cd(nomWeb)
+  shell.exec('git checkout ' + commitId)
+  res.send('Restaurat commit ' + commitId + ' de la web ' + nomWeb + ' fet!')
+  console.log('==========================\n')
 })
 
-router.post('/backup', function(req, res) {
-	console.log(req.body)
-	let full_path = req.body.path
-	let nom_web = check(full_path, req, res)
-	let command_cd = shell.cd(nom_web)
-	let date = shell.exec('date')
-	let commit_message = 'Commit fet el '+date
-	let command_git_add_all = shell.exec('git add -A')
-	let command_git_commit_all = shell.exec('git commit -a -m "'+commit_message+'"')
-	res.send('Backup a '+nom_web+' fet!')
+router.post('/backup_web', function (req, res) {
+  console.log('\n==========================\n')
+  console.log('## REALITZANT BACKUP   ##\n')
+  console.log('==========================\n')
+  console.log(req.body)
+  let fullPath = req.body.path
+  let nomWeb = check(fullPath, req, res)
+  shell.cd(nomWeb)
+  let date = shell.exec('date')
+  let commitMissatge = 'Commit fet el ' + date
+  shell.exec('git add -A')
+  shell.exec('git commit -a -m "' + commitMissatge + '"')
+  res.send('Backup a ' + nomWeb + ' fet!')
+  console.log('==========================\n')
 })
 
-router.post('/undo_restore', function(req, res) {
-	let full_path = req.body.path
-	let nom_web = check(full_path, req, res)
-	let command_cd = shell.cd(nom_web)
-	let command_git_checkout_master = shell.exec('git checkout master')
-	res.send('Desfet el restaurar versio de la web '+nom_web)
+router.post('/undo_restore_web', function (req, res) {
+  console.log('\n=============================\n')
+  console.log('\n## REALITZANT UNDO RESTORE ##\n')
+  console.log('\n=============================\n')
+  let fullPath = req.body.path
+  let nomWeb = check(fullPath, req, res)
+  shell.cd(nomWeb)
+  shell.exec('git checkout master')
+  res.send('Desfet el restaurar versio de la web ' + nomWeb)
+  console.log('==========================\n')
 })
+
+// FILE ROUTES
+router.post('/restore_file', function (req, res) {
+  console.log('\n==========================\n')
+  console.log('## REALITZANT RESTORE   ##\n')
+  console.log('==========================\n')
+  let fullPath =  GLOBAL_PATH + '/' + req.body.nom_web
+  let fileName = req.body.file_name
+  let commitId = req.body.commit_id
+  let nomWeb = check(fullPath, req, res)
+  shell.cd(nomWeb)
+  shell.exec('git checkout '+commitId+' ' + fileName)
+  console.log('comanda a fer: '+'git checkout '+commitId+' ' + fileName)
+  res.send('Restaurat commit ' + commitId + ' del fitxer ' + fileName + ' la web ' + nomWeb + ' fet!')
+  console.log('==========================\n')
+})
+
 
 // HOME
 router.get('/', function (req, res, next) {
-  let list_webs = shell.exec('ls '+GLOBAL_PATH).split('\n').slice(0, -1)
-  console.log('### llistat de webs: ' + list_webs)
-  res.locals.list_webs = list_webs
+  let listWebs = shell.exec('ls ' + GLOBAL_PATH).split('\n').slice(0, -1)
+  console.log('### llistat de webs: ' + listWebs)
+  res.locals.list_webs = listWebs
   next()
 })
 
