c8bbecc6dcfedf819abf2fe8a55f116fdf9191e3
Oriol Testart <orioltestart@gmail.com>
2017-01-20 20:15:52 +0100

    Update

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const path = require('path')
 const app = express()
 const body_parser = require('body-parser')
 const shell = require('shelljs')
+const cron = require('node-cron')
+const colors = require('colors')
+
+cron.schedule('* * * * *', function() {
+  console.log(colors.red("Every minute!!"))
+})
 
 //  Configure app
 
@@ -15,7 +21,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, '/public')))
 app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')))
-app.use(body_parser())
+app.use(body_parser.urlencoded({ extended: true }))
+app.use(body_parser.json())
 //  Rutas
 
 app.use('/', require('./routes/index'))
